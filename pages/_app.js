5fa78438c3b480f4b22a8eac6ab54f9d0b7d4c32
Rafid Aslam (refeed) <rafidteam@gmail.com>
2022-06-09 15:09:53 +0700

    Comply with standardjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
--- pages/api/submitPesan.js
+++ pages/api/submitPesan.js
@@ -1,26 +1,26 @@
-import axios from "axios";
+import axios from 'axios'
 
 // REST_DB_API_KEY must be submitted in ?apikey
 // or headers: { 'x-apikey': apikey }
-const REST_DB_API_KEY = "927070e142276a1edce544d5eec84fca748cf";
-const BASE_URL = "https://refeedsertifikasi-0abb.restdb.io/";
+const REST_DB_API_KEY = '927070e142276a1edce544d5eec84fca748cf'
+const BASE_URL = 'https://refeedsertifikasi-0abb.restdb.io/'
 
 const restdb = axios.create({
   baseURL: BASE_URL,
-  headers: { "x-apikey": REST_DB_API_KEY }
-});
+  headers: { 'x-apikey': REST_DB_API_KEY }
+})
 
-export default function handler(req, res) {
+export default function handler (req, res) {
   restdb
-    .post("/rest/busorder", {
+    .post('/rest/busorder', {
       ...req.body
     })
     .then(restdbRes => {
       console.log(restdbRes)
       if (restdbRes.status === 201) {
-        res.status(200).json({ status: "Order success" });
-        return;
+        res.status(200).json({ status: 'Order success' })
+        return
       }
-      res.status(500).json({ status: "Order failed" });
-    });
+      res.status(500).json({ status: 'Order failed' })
+    })
 }
